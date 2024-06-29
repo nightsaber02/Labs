d35c9acb0705a9cdcc9b05347e33dfcddca69dbe
nightsaber02 <60774295+nightsaber02@users.noreply.github.com>
2024-06-29 15:44:18 +0300

    Create output_2.py

---
+++ Lab4/output_2.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+class Square(Shape):
+    def __init__(self, side):
+        self.side = side
+
+    def area(self):
+        return self.side ** 2
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius ** 2
+
+# Приклад використання класів
+square = Square(5)
+print("Area of square:", square.area())
+
+circle = Circle(3)
+print("Area of circle:", circle.area())
